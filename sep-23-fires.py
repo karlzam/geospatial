59053d738198715079cb36234c5772880f1f9eee
karlee-zammit <karlee.zammit@gmail.com>
2024-09-18 10:58:07 -0700

    Working on reproducing QGIS work in Python

--- sep-23-fires.py
+++ sep-23-fires.py
@@ -4,6 +4,7 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from pyproj import Proj
+from pyproj import CRS
 
 from osgeo import gdal
 gdal.SetConfigOption('SHAPE_RESTORE_SHX', 'YES')
@@ -18,9 +19,19 @@ ab_bc_ma_map = ca_map[(ca_map['name']=='Alberta') | (ca_map['name']=='British Co
 # This is in easting northing
 # Current CRS is 3978 (NAD83_NRCan_LCC_Canada)
 subset_poly = gpd.read_file(r'C:\Users\Karlee\Documents\NFC\Sep23-Fire\subset-polygon.shp')
-subset_poly = gpd.GeoDataFrame(subset_poly)
-#gdf = gdf.set_crs(epsg=3857)
+# This converts the lat/long properly for plotting
+# EPSG 4326 is a geographic coordinate system
 subset_poly = subset_poly.to_crs(epsg=4326)
+
+# To create a buffer we first need to project into a projected coordinate system
+# 32610 is UTM 10N
+# Q: What do when data spans multiple UTM zones?
+# Project to a specific CRS so the buffer can be applied
+gdf_projected = subset_poly.to_crs(CRS(32610))
+buffer_distance = 5000  # 5 km in meters
+#gdf_buffered = gdf_projected.copy()
+#gdf_buffered['geometry'] = gdf_projected.buffer(buffer_distance)
+#gdf_buffered = gdf_buffered.to_crs(epsg=4326)
 
 # Get csv of all Alberta fires from Sep 23
 fires_sep23 = pd.read_csv(r'C:\Users\Karlee\Documents\NFC\Sep23-Fire\alberta-fires-sep23.csv')
@@ -29,6 +40,8 @@ df_fs23 = gpd.GeoDataFrame(fires_sep23, geometry=gpd.points_from_xy(fires_sep23.
 # Get persistent hot spots
 # These are in easting, northing
 persistent_hs = gpd.read_file(r'C:\Users\Karlee\Documents\NFC\Sep23-Fire\m3mask5_lcc.shp')
+persistent_hs_sub = persistent_hs[(persistent_hs['prov']=='AB') | (persistent_hs['prov']=='BC') | (persistent_hs['prov']=='SK')]
+persistent_hs_sub = persistent_hs_sub.to_crs(epsg=4326)
 
 ## Plot the background map
 fig, ax = plt.subplots(figsize=(10,8))
@@ -37,10 +50,13 @@ ab_bc_ma_map.plot(ax=ax, edgecolor='black', linewidth=1, cmap='Greens')
 ax.set_facecolor('#a2d2ff')
 # plot the subset polygon
 subset_poly.plot(ax=ax)
+# plot the buffer polygon
+#gdf_buffered.plot(ax=ax)
 # Plot the alberta fires from Sep 23 2023
-df_fs23.plot(ax=ax, color='red', marker='*', markersize=10)
-#persistent_hs.plot(ax=ax, color='black')
-plt.show()
+df_fs23.plot(ax=ax, color='red', marker='*', markersize=5)
+persistent_hs_sub.plot(ax=ax, color='black')
+# plt.show()
+plt.savefig('test.png')
 
 print('test')
 
