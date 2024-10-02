162bc32fe1e209deefccd41abe16195ea40b5741
karlzam <karlee.zammit@gmail.com>
2024-10-02 16:45:21 -0700

    More work on plume script & added script to pull VIIRS data

---
+++ dev-script.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import netCDF4
+import xarray as xr
+
+# This one works
+fp=r'C:\Users\kzammit\Documents\FWIq95_1991_2020.nc'
+
+# These are weird - from download script
+# https://lpdaac.usgs.gov/products/vnp14imgv002/
+# fp = r'C:\Users\kzammit\Documents\VIIRS-data-VNP14IMG\2018\207\VNP14IMG.A2018207.2100.002.2024081023159.nc'
+
+# Downloaded this directly from earth data
+fp = r'C:\Users\kzammit\Documents\VIIRS-data-VNP14IMG\Direct\VNP14IMG.A2018207.2100.001.2018208043638.nc'
+
+nc = netCDF4.Dataset(fp)
+
+ds = xr.open_dataset(fp)
+
+#ds['fire mask'].plot()
+#plt.savefig('test.png')
+
+plt.imshow(ds['fire mask'])
+plt.show()
+
+print('test')
