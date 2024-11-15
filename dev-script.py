a9a5b72898a0453c64825deaaa28e29a34ac974e
karlzam <karlee.zammit@gmail.com>
2024-11-15 12:17:38 -0800

    work on DL chapter

--- dev-script.py
+++ dev-script.py
@@ -1,13 +1,99 @@
-import os, gzip, shutil
-dir_name = r'C:\Users\kzammit\Documents\SAFD'
-def gz_extract(directory):
-    extension = ".gz"
-    os.chdir(directory)
-    for item in os.listdir(directory): # loop through items in dir
-      if item.endswith(extension): # check for ".gz" extension
-          gz_name = os.path.abspath(item) # get full path of files
-          file_name = (os.path.basename(gz_name)).rsplit('.',1)[0] #get file name for file within
-          with gzip.open(gz_name,"rb") as f_in, open(file_name,"wb") as f_out:
-              shutil.copyfileobj(f_in, f_out)
-          os.remove(gz_name) # delete zipped file
-gz_extract(dir_name)
+import geopandas as gpd
+import pandas as pd
+import datetime as dt
+from shapely.geometry import MultiPolygon, Polygon
+import matplotlib.pyplot as plt
+from shapely.plotting import plot_polygon
+
+# Define the fire IDs of interest
+fire_ids = ['2023_348', '2023_345', '2023_203', '2023_207', '2023_854', '2023_834']
+
+# Load the shapefile containing fire data
+nbac_shapefile = r'C:\Users\kzammit\Documents\Shapefiles\NBAC\nbac_2023_20240530.shp'
+nbac = gpd.read_file(nbac_shapefile)
+
+# Convert the CRS to EPSG:4326 for compatibility with FIRMS data
+nbac = nbac.to_crs('EPSG:4326')
+
+# Filter for the fires of interest
+nbac_filtered = nbac[nbac['GID'].isin(fire_ids)].copy()
+
+# Add a column with bounding box coordinates for each fire
+nbac_filtered['bbox_coords'] = nbac_filtered['geometry'].apply(lambda geom: geom.bounds)
+
+# FIRMS API setup (replace with your own MAP_KEY)
+MAP_KEY = 'e865c77bb60984ab516517cd4cdadea0'
+url = f'https://firms.modaps.eosdis.nasa.gov/mapserver/mapkey_status/?MAP_KEY={MAP_KEY}'
+
+# Test connection to FIRMS API
+try:
+    df = pd.read_json(url, typ='series')
+except Exception as e:
+    print(f"Error connecting to FIRMS API: {e}")
+    print(f"Check URL in your browser: {url}")
+
+# Loop over each fire in the filtered dataset
+for idx, fire in nbac_filtered.iterrows():
+    # Convert fire start and end dates to datetime objects
+    end_date = dt.datetime.strptime(fire['HS_EDATE'], "%Y/%m/%d").date()
+    start_date = dt.datetime.strptime(fire['HS_SDATE'], "%Y/%m/%d").date()
+    delta = end_date - start_date
+
+    # Calculate the number of 10-day intervals
+    num_intervals = delta.days // 10
+
+    # Generate a list of dates for each 10-day interval
+    date_list = [end_date - dt.timedelta(days=10 * i) for i in range(num_intervals + 1)]
+    date_list.reverse()  # To start with the most recent date
+
+    # Get the bounding box coordinates for the current fire
+    bbox_coords = fire['bbox_coords']
+    coords_str = f"{bbox_coords[0]},{bbox_coords[1]},{bbox_coords[2]},{bbox_coords[3]}"
+
+    # Initialize dataframes for storing hotspots data
+    df_SNPP_all = pd.DataFrame()
+    df_MODIS_all = pd.DataFrame()
+
+    # Loop over the date list to download hotspot data for each 10-day period
+    for i, date in enumerate(date_list):
+        # Determine the number of days for the current period (last period might be less than 10 days)
+        period_days = 10 if i < len(date_list) - 1 else delta.days % 10
+
+        # Construct the FIRMS API URLs for VIIRS and MODIS
+        url_SNPP = f'https://firms.modaps.eosdis.nasa.gov/api/area/csv/{MAP_KEY}/VIIRS_SNPP_SP/{coords_str}/{period_days}/{date}'
+        url_MODIS = f'https://firms.modaps.eosdis.nasa.gov/api/area/csv/{MAP_KEY}/MODIS_SP/{coords_str}/{period_days}/{date}'
+
+        # Read the hotspot data from the API into pandas dataframes
+        df_SNPP = pd.read_csv(url_SNPP)
+        df_MODIS = pd.read_csv(url_MODIS)
+
+        # Concatenate the new data to the existing dataframe
+        df_SNPP_all = pd.concat([df_SNPP_all, df_SNPP], ignore_index=True)
+        df_MODIS_all = pd.concat([df_MODIS_all, df_MODIS], ignore_index=True)
+
+    # Convert the SNPP and MODIS dataframes to GeoDataFrames
+    gdf_SNPP = gpd.GeoDataFrame(df_SNPP_all, geometry=gpd.points_from_xy(df_SNPP_all.longitude, df_SNPP_all.latitude), crs="EPSG:4326")
+    gdf_MODIS = gpd.GeoDataFrame(df_MODIS_all, geometry=gpd.points_from_xy(df_MODIS_all.longitude, df_MODIS_all.latitude), crs="EPSG:4326")
+
+    # Load the map of Canada (or other region of interest)
+    map_path = r'C:\Users\kzammit\Documents\Shapefiles\Natural-Earth\ne_10m_admin_1_states_provinces.shp'
+    map_data = gpd.read_file(map_path)
+    ca_map = map_data[map_data['iso_a2'] == 'CA']
+    ca_map_proj = ca_map.to_crs(epsg=4326)
+
+    # Plot the data
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ca_map_proj.plot(ax=ax, edgecolor='white', linewidth=1, color='black')
+    plot_polygon(fire['geometry'], ax=ax, color='white', zorder=1)
+    gdf_SNPP.plot(ax=ax, color='red', zorder=2)
+    gdf_MODIS.plot(ax=ax, color='orange', zorder=3)
+
+    # Set the plot limits based on the fire's bounding box
+    ax.set_xlim(float(coords_str.split(',')[0]), float(coords_str.split(',')[2]))
+    ax.set_ylim(float(coords_str.split(',')[1]), float(coords_str.split(',')[3]))
+
+    # Save the plot to a file
+    output_path = r'C:\Users\kzammit\Documents\DL-chapter\hotspots-fire-' + str(fire['GID']) + '.png'
+    plt.savefig(output_path)
+
+    print(f"Saved plot for fire {fire['GID']} to {output_path}")
--- dl-chapter.py
+++ dl-chapter.py
@@ -1,88 +1,102 @@
 import geopandas as gpd
 import pandas as pd
 import datetime as dt
-from shapely.geometry import MultiPolygon, Polygon
 import matplotlib.pyplot as plt
+from shapely.plotting import plot_polygon
 
-ids = ['2023_348', '2023_345', '2023_203', '2023_207', '2023_854', '2023_834']
-nbac = gpd.read_file(r'C:\Users\kzammit\Documents\Shapefiles\NBAC\nbac_2023_20240530.shp')
+# Define the fire IDs of interest
+fire_ids = ['2023_348', '2023_345', '2023_203', '2023_207', '2023_854', '2023_834']
 
-# Convert to EPSG 4326 as this is what we will pull FIRMS data with
+# Load the shapefile containing fire data
+nbac_shapefile = r'C:\Users\kzammit\Documents\Shapefiles\NBAC\nbac_2023_20240530.shp'
+nbac = gpd.read_file(nbac_shapefile)
+
+# Convert the CRS to EPSG:4326 for compatibility with FIRMS data
 nbac = nbac.to_crs('EPSG:4326')
 
-# Filter so we're only looking at the fires of interest (the copy is so we're not altering the original dataframe)
-nbac_filtered = nbac[nbac['GID'].isin(ids)].copy()
+# Filter for the fires of interest
+nbac_filtered = nbac[nbac['GID'].isin(fire_ids)].copy()
 
-# Add a new column that is a bounding box for each fire
-# This will be used to pull FIRMS data just for the fire of interest
+# Add a column with bounding box coordinates for each fire
 nbac_filtered['bbox_coords'] = nbac_filtered['geometry'].apply(lambda geom: geom.bounds)
 
-# Set up the FIRMS API
-# go here to get your own: https://firms.modaps.eosdis.nasa.gov/api/map_key
-FIRMS_map_key = 'e865c77bb60984ab516517cd4cdadea0'
-MAP_KEY = FIRMS_map_key
-url = 'https://firms.modaps.eosdis.nasa.gov/mapserver/mapkey_status/?MAP_KEY=' + MAP_KEY
+# FIRMS API setup (replace with your own MAP_KEY)
+MAP_KEY = 'e865c77bb60984ab516517cd4cdadea0'
+url = f'https://firms.modaps.eosdis.nasa.gov/mapserver/mapkey_status/?MAP_KEY={MAP_KEY}'
+
+# Test connection to FIRMS API
 try:
     df = pd.read_json(url, typ='series')
-except:
-    # possible error, wrong MAP_KEY value, check for extra quotes, missing letters
-    print("There is an issue with the query. \nTry in your browser: %s" % url)
+except Exception as e:
+    print(f"Error connecting to FIRMS API: {e}")
+    print(f"Check URL in your browser: {url}")
 
+# Loop over each fire in the filtered dataset
 for idx, fire in nbac_filtered.iterrows():
-
-    # no days goes backwards from the last day
+    # Convert fire start and end dates to datetime objects
     end_date = dt.datetime.strptime(fire['HS_EDATE'], "%Y/%m/%d").date()
     start_date = dt.datetime.strptime(fire['HS_SDATE'], "%Y/%m/%d").date()
     delta = end_date - start_date
-    #no_days = delta.days
-    # TODO: Edit this as the maximum no days is 10, so need to concat dfs for the full date range
-    # remember it's going backwards
-    no_days = 10
 
-    # date to pull, and those before that date defined by no_days
-    date = end_date
+    # Calculate the number of 10-day intervals
+    num_intervals = delta.days // 10
 
-    # define bounding box of area of interest
+    # Generate a list of dates for each 10-day interval
+    date_list = [end_date - dt.timedelta(days=10 * i) for i in range(num_intervals + 1)]
+    date_list.reverse()  # To start with the most recent date
 
-    coords = (str(fire['bbox_coords'][0]) + ',' + str(fire['bbox_coords'][1]) + ','
-              + str(fire['bbox_coords'][2]) + ',' + str(fire['bbox_coords'][3]))
+    # Get the bounding box coordinates for the current fire
+    bbox_coords = fire['bbox_coords']
+    coords_str = f"{bbox_coords[0]},{bbox_coords[1]},{bbox_coords[2]},{bbox_coords[3]}"
 
-    #coords = (str(fire['bbox_coords'][2]) + ',' + str(fire['bbox_coords'][1]) + ','
-    #          + str(fire['bbox_coords'][0]) + ',' + str(fire['bbox_coords'][3]))
+    # Initialize dataframes for storing hotspots data
+    df_SNPP_all = pd.DataFrame()
+    df_MODIS_all = pd.DataFrame()
 
-    ## VIIRS_S-NPP_SP
-    # Create the corresponding URL for the fire
-    area_url_SNPP = ('https://firms.modaps.eosdis.nasa.gov/api/area/csv/' + MAP_KEY + '/VIIRS_SNPP_SP/' +
-                     str(coords) + '/' + str(no_days) + '/' + str(date))
+    # Loop over the date list to download hotspot data for each 10-day period
+    for i, date in enumerate(date_list):
+        # Determine the number of days for the current period (last period might be less than 10 days)
+        period_days = 10 if i < len(date_list) - 1 else delta.days % 10
 
-    # Read the hotspots as a pandas dataframe
-    df_SNPP = pd.read_csv(area_url_SNPP)
+        # Construct the FIRMS API URLs for VIIRS and MODIS
+        url_SNPP = f'https://firms.modaps.eosdis.nasa.gov/api/area/csv/{MAP_KEY}/VIIRS_SNPP_SP/{coords_str}/{period_days}/{date}'
+        url_MODIS = f'https://firms.modaps.eosdis.nasa.gov/api/area/csv/{MAP_KEY}/MODIS_SP/{coords_str}/{period_days}/{date}'
 
-    # Convert to a geopandas dataframe and make sure we're using the same crs as the bounding boxes
-    gdf_SNPP = gpd.GeoDataFrame(
-        df_SNPP, geometry=gpd.points_from_xy(df_SNPP.longitude, df_SNPP.latitude), crs="EPSG:4326"
-    )
+        # Read the hotspot data from the API into pandas dataframes
+        df_SNPP = pd.read_csv(url_SNPP)
+        df_MODIS = pd.read_csv(url_MODIS)
 
-    ## MODIS_SP
-    area_url_MODIS = ('https://firms.modaps.eosdis.nasa.gov/api/area/csv/' + MAP_KEY + '/MODIS_SP/' +
-                     str(coords) + '/' + str(no_days) + '/' + str(date))
+        # Filter out columns that are entirely NaN or empty before concatenating
+        df_SNPP = df_SNPP.dropna(axis=1, how='all')
+        df_MODIS = df_MODIS.dropna(axis=1, how='all')
 
-    # Read the hotspots as a pandas dataframe
-    df_MODIS = pd.read_csv(area_url_MODIS)
+        # Concatenate the new data to the existing dataframe
+        df_SNPP_all = pd.concat([df_SNPP_all, df_SNPP], ignore_index=True)
+        df_MODIS_all = pd.concat([df_MODIS_all, df_MODIS], ignore_index=True)
 
-    # Convert to a geopandas dataframe and make sure we're using the same crs as the bounding boxes
-    gdf_MODIS = gpd.GeoDataFrame(
-        df_MODIS, geometry=gpd.points_from_xy(df_MODIS.longitude, df_MODIS.latitude), crs="EPSG:4326"
-    )
+    # Convert the SNPP and MODIS dataframes to GeoDataFrames
+    gdf_SNPP = gpd.GeoDataFrame(df_SNPP_all, geometry=gpd.points_from_xy(df_SNPP_all.longitude, df_SNPP_all.latitude), crs="EPSG:4326")
+    gdf_MODIS = gpd.GeoDataFrame(df_MODIS_all, geometry=gpd.points_from_xy(df_MODIS_all.longitude, df_MODIS_all.latitude), crs="EPSG:4326")
 
-    print('test')
-    # Double checking this is in the right region
-    map = gpd.read_file(r'C:\Users\kzammit\Documents\Shapefiles\Natural-Earth\ne_10m_admin_1_states_provinces.shp')
-    ca_map = map[map['iso_a2'] == 'CA']
+    # Load the map of Canada (or other region of interest)
+    map_path = r'C:\Users\kzammit\Documents\Shapefiles\Natural-Earth\ne_10m_admin_1_states_provinces.shp'
+    map_data = gpd.read_file(map_path)
+    ca_map = map_data[map_data['iso_a2'] == 'CA']
     ca_map_proj = ca_map.to_crs(epsg=4326)
-    fig, ax = plt.subplots(figsize=(10, 8))
-    ca_map_proj.plot(ax=ax, edgecolor='white', linewidth=1, color='Black')
-    gdf_SNPP.plot(ax=ax)
-    plt.savefig('test.png')
 
-    print('test')
+    # Plot the data
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ca_map_proj.plot(ax=ax, edgecolor='white', linewidth=1, color='black')
+    plot_polygon(fire['geometry'], ax=ax, color='white', zorder=1)
+    gdf_SNPP.plot(ax=ax, color='red', zorder=2)
+    gdf_MODIS.plot(ax=ax, color='orange', zorder=3)
+
+    # Set the plot limits based on the fire's bounding box
+    ax.set_xlim(float(coords_str.split(',')[0]), float(coords_str.split(',')[2]))
+    ax.set_ylim(float(coords_str.split(',')[1]), float(coords_str.split(',')[3]))
+
+    # Save the plot to a file
+    output_path = r'C:\Users\kzammit\Documents\DL-chapter\hotspots-fire-' + str(fire['GID']) + '.png'
+    plt.savefig(output_path)
+
+    print(f"Saved plot for fire {fire['GID']} to {output_path}")
