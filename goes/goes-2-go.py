c4b3372161ab9831c99fd58bb5e0faa02b3f1a47
karlzam <karlee.zammit@gmail.com>
2025-02-17 16:16:49 -0800

    trying more SSL related things but failing :(

--- goes/goes-2-go.py
+++ goes/goes-2-go.py
@@ -1,17 +1,17 @@
 from goes2go import GOES
 from goes2go.data import goes_nearesttime
 
-from datetime import datetime, timedelta
-import pandas as pd
-import boto3
-import botocore
+#from datetime import datetime, timedelta
+#import pandas as pd
+#import boto3
+#import botocore
 
-session = boto3.session.Session()
-s3_client = session.client(
-    'noaa-goes16.s3.amazonaws.com',
-    config=botocore.config.Config(signature_version=botocore.UNSIGNED, retries={'max_attempts': 3}),
-    verify=False  # Disable SSL verification (use with caution)
-)
+#session = boto3.session.Session()
+#s3_client = session.client(
+#    'noaa-goes16.s3.amazonaws.com',
+#    config=botocore.config.Config(signature_version=botocore.UNSIGNED, retries={'max_attempts': 3}),
+#    verify=False  # Disable SSL verification (use with caution)
+#)
 
 G = GOES(satellite=16, product="ABI-L2-MCMIP", domain='C')
 
