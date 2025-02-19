5c7c317e8ca776809eba910214b95cd7f2fb4e3c
karlzam <karlee.zammit@gmail.com>
2025-02-19 15:29:17 -0800

    trying out this app

--- dev-script.py
+++ dev-script.py
@@ -1,24 +1,2 @@
-import pandas as pd
-import geopandas as gpd
-import matplotlib.pyplot as plt
-from datetime import datetime
-import numpy as np
-from networkx import Graph, connected_components
-from scipy.spatial import KDTree
-from sklearn.cluster import DBSCAN
-from shapely.geometry import MultiPoint, MultiPolygon, Polygon
-import alphashape
-import re
-
-file = pd.read_excel(r'C:\Users\kzammit\Documents\plumes\dfs\all-false-positives-2023-09-24.xlsx')
-
-perim_points = [[float(lon), float(lat)] for lon, lat in re.findall(r"(-?\d+\.\d+)\s+(-?\d+\.\d+)", file['fire-perimeter'][0])]
-poly = Polygon(perim_points)
-x, y = poly.exterior.xy
-
-fig, ax = plt.subplots(figsize=(10,8))
-plt.plot(x, y, color='blue')
-plt.fill(x, y, color='lightblue', alpha=0.5)
-
-plt.savefig('test.png')
-
+import ee
+ee.Authenticate(force=True)
--- goes/goes-2-go.py
+++ goes/goes-2-go.py
@@ -1,27 +1,49 @@
 from goes2go import GOES
 from goes2go.data import goes_nearesttime
+from goes2go.data import goes_timerange
 from datetime import datetime, timedelta
-#import pandas as pd
-#import boto3
-#import botocore
 from goes2go.data import goes_latest
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import pandas as pd
+from timezonefinder import TimezoneFinder
+import pytz
+
+# later draw the box on top of the file
+fires = pd.read_excel(r'C:\Users\kzammit\Documents\Plumes\all-false-positives-manual-2023-09-23.xlsx')
+
+# get the first coordinate from "bounds" (which is in EPSG4326 from the script)
+# check what the conversion is for that timezone from UTC
+# figure out what time 1:30 pm local is in UTC
+
+# coord is long, lat
+coord_1 = fires['bounds'][0].split(',')[0].split('(')[1]
+coord_2 = fires['bounds'][0].split(',')[1].split(' ')[1]
+
+# get UTC time for 1:00 pm local
+tf = TimezoneFinder()
+timezone = tf.timezone_at(lng=float(coord_1), lat=float(coord_2))
+local_tz = pytz.timezone(timezone)
+local_time = local_tz.localize(datetime(2023, 9, 23, 10, 0))  # 1 PM
+utc_time = local_time.astimezone(pytz.UTC)
+
+print(f"Local Time: {local_time}")
+print(f"UTC Time: {utc_time}")
+
+end_time = utc_time + timedelta(hours=16)
+start = utc_time.strftime("%Y-%m-%d %H:%M")
+end = end_time.strftime("%Y-%m-%d %H:%M")
+
+#g = goes_timerange(start, end,
+#                   satellite='goes16',
+#                   product='ABI',
+#                   return_as='filelist', save_dir=r'C:\Users\kzammit\Documents\GOES')
+
+g = goes_timerange(start, end,
+                   satellite='goes16',
+                   product='ABI',
+                   return_as='xarray', save_dir=r'C:\Users\kzammit\Documents\GOES')
 
 
-g = goes_nearesttime(datetime(2020, 12, 25, 10),
-                     satellite='goes16',
-                     product='ABI',
-                     return_as='xarray',
-                     save_dir=r'C:\Users\kzammit\Documents\GOES')
+print('test')
 
-# Download a GOES ABI dataset
-#G = goes_latest(product='ABI')
-
-# Make figure on Cartopy axes
-#ax = plt.subplot(projection=G.rgb.crs)
-ax = plt.subplot(projection=g.rgb.crs)
-#ax.imshow(G.rgb.TrueColor(), **G.rgb.imshow_kwargs)
-ax.imshow(g.rgb.TrueColor(), **g.rgb.imshow_kwargs)
-ax.coastlines()
-plt.savefig(r'C:\Users\kzammit\Documents\GOES\test.png')
